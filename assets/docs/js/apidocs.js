1cab5a64901a731b6200d3a7d83cc2186dacc9b2
UserGhost411 <UserGhost411@yahoo.com>
2022-10-29 03:40:35 +0700

    add tagging style

--- assets/docs/js/apidocs.js
+++ assets/docs/js/apidocs.js
@@ -30,7 +30,18 @@ $(window).on('load', function () {
         for (const schema of cfg.schemes) {
             $("#api_schema").append(new Option(schema.toUpperCase(), schema)).change();
         }
+        for (const tag of cfg.tags) {
+            $("#api_endpoints").append(`
+            <div class="card mt-3">
+                <div class="card-body" id="tag_${tag.name.hashCode()}">
+                    <span class="tag-name">${tag.name}</span> <small>${tag.description ? tag.description : ''}</small>
+                    <hr>
+                </div>
+            </div>
+            `);
+        }
         let endpoint_id = 0;
+        let unique_hash = 0;
         for (const endpoint in cfg.paths) {
             let endpoint_methods = cfg.paths[endpoint];
             for (const method in endpoint_methods) {
@@ -43,48 +54,69 @@ $(window).on('load', function () {
                     }
                 )
                 endpoint_id += 1;
-                $("#api_endpoints").append(`
-                <div class="card api-summary ${method}">
-                    <div class="card-body">
-                        <span class="api-summary-method">${method.toUpperCase()}</span>
-                        <span class="api-summary-path">${endpoint}</span>
-                        <span class="api-summary-btn collapsed" data-toggle="collapse" data-target="#ep_path${endpoint_id}"><i class="fa" aria-hidden="true"></i></span>
-                        <div id="ep_path${endpoint_id}" class="collapse">
-                            <hr>
-                            <div class="mx-2">
-                                <p>${endpoint_methods[method].summary}</p>
-                                ${converter.makeHtml(endpoint_methods[method].description)}
-                            </div>
-                            <table class="table">
-                            <thead>
-                              <tr>
-                                <th>Parameters</th>
-                                <th class="desc_endpoint">Description
-                                <button class="btn btn-primary btn-sm pull-right tryapi" attr-api-index="${(endpoint_id - 1)}">Try API <i class="fa fa-chevron-right"></i></button>
-                                </th>
-                              </tr>
-                            </thead>
-                            <tbody id="ep_vars${endpoint_id}"></tbody>
-                          </table>
-                        </div>
-                    </div>
-                </div>
-                `);
-                for (const param of endpoint_methods[method].parameters) {
-
-                    $(`#ep_vars${endpoint_id}`).append(`<tr>
-                        <td>
-                            ${param.required ? `<b>${param.name}</b>` : param.name} ${param.required ? '<code>*Required</code>' : ''}<br>
-                            <pre class="mb-0">${param.type ? param.type : 'Object'}</pre>
-                            <small class="text-muted">(<i>${param.in}</i>)</small>
-                        </td>
-                        <td>
-                            ${converter.makeHtml(param.description)}
-                            ${param.type ? (param.type == 'array' && param.items.enum ? `<br>[${param.items.enum.join(",")}]` : '') : ''}
-                        </td>
-                    </tr>`)
+                if (endpoint_methods[method].tags.length > 0) {
+                    for (const tag of endpoint_methods[method].tags) {
+                        unique_hash += (Math.floor(Math.random() * 10) + 1);
+                        addendpoint(tag.hashCode(), endpoint_id, unique_hash, method, endpoint, endpoint_methods[method]);//data=endpoint_methods[method]
+                    }
+                } else {
+                    unique_hash += (Math.floor(Math.random() * 10) + 1);
+                    addendpoint('c21f969b5f03d33d43e04f8f136e7682', endpoint_id, unique_hash, method, endpoint, endpoint_methods[method]);//data=endpoint_methods[method]
                 }
             }
+        }
+    }
+    function addendpoint(tagid, endpoint_id, unique_hash, method, endpoint, data) { //data=endpoint_methods[method]
+        if (!$(`#tag_${tagid}`).length) {
+            $("#api_endpoints").append(`
+            <div class="card mt-3">
+                <div class="card-body" id="tag_c21f969b5f03d33d43e04f8f136e7682">
+                    <span class="tag-name">Default</span>
+                    <hr>
+                </div>
+            </div>
+            `);
+            tagid = "c21f969b5f03d33d43e04f8f136e7682";
+        }
+        $(`#tag_${tagid}`).append(`
+            <div class="card api-summary ${method}">
+                <div class="card-body">
+                    <span class="api-summary-method">${method.toUpperCase()}</span>
+                    <span class="api-summary-path">${endpoint}</span>
+                    <span class="api-summary-btn collapsed" data-toggle="collapse" data-target="#ep_path${unique_hash}"><i class="fa" aria-hidden="true"></i></span>
+                    <div id="ep_path${unique_hash}" class="collapse">
+                        <hr>
+                        <div class="mx-2">
+                            <p>${data.summary}</p>
+                            ${converter.makeHtml(data.description)}
+                        </div>
+                        <table class="table">
+                        <thead>
+                        <tr>
+                            <th>Parameters</th>
+                            <th class="desc_endpoint">Description
+                            <button class="btn btn-primary btn-sm pull-right tryapi" attr-api-index="${(endpoint_id - 1)}">Try API <i class="fa fa-chevron-right"></i></button>
+                            </th>
+                        </tr>
+                        </thead>
+                        <tbody id="ep_vars${unique_hash}"></tbody>
+                    </table>
+                    </div>
+                </div>
+            </div>
+        `);
+        for (const param of data.parameters) {
+            $(`#ep_vars${unique_hash}`).append(`<tr>
+                <td>
+                    ${param.required ? `<b>${param.name}</b>` : param.name} ${param.required ? '<code>*Required</code>' : ''}<br>
+                    <pre class="mb-0">${param.type ? param.type : 'Object'}</pre>
+                    <small class="text-muted">(<i>${param.in}</i>)</small>
+                </td>
+                <td>
+                    ${converter.makeHtml(param.description)}
+                    ${param.type ? (param.type == 'array' && param.items.enum ? `<br>[${param.items.enum.join(",")}]` : '') : ''}
+                </td>
+            </tr>`)
         }
     }
     $("#api_endpoints").on('click', '.tryapi', function () {
@@ -166,9 +198,9 @@ $(window).on('load', function () {
             type: endpoint_data.method,
             cache: false,
         }).done(function (data, status, jqXHR) {
-            try{
+            try {
                 $("#text_resp_modal").val(JSON.stringify(data, null, 2));
-            } catch(err){
+            } catch (err) {
                 $("#text_resp_modal").val(data);
             }
             $("#param_modal_head_res").append(`<tr><td>status</td><td>${jqXHR.status} ${jqXHR.statusText.trim()}</td></tr>`)
@@ -178,9 +210,9 @@ $(window).on('load', function () {
                 $("#param_modal_head_res").append(`<tr><td>${head[0].trim()}</td><td>${head[1].trim()}</td></tr>`)
             }
         }).fail(function (jqXHR, status) {
-            try{
+            try {
                 $("#text_resp_modal").val(((jqXHR.status > 0) ? JSON.stringify(JSON.parse(jqXHR.responseText), null, 2) : status));
-            } catch(err){
+            } catch (err) {
                 $("#text_resp_modal").val(((jqXHR.status > 0) ? jqXHR.responseText : status));
             }
             $("#param_modal_head_res").append(`<tr><td>status</td><td>${jqXHR.status} ${jqXHR.statusText.trim()}</td></tr>`)
@@ -196,3 +228,14 @@ $(window).on('load', function () {
         e.preventDefault();
     });
 });
+String.prototype.hashCode = function () {
+    var hash = 0,
+        i, chr;
+    if (this.length === 0) return hash;
+    for (i = 0; i < this.length; i++) {
+        chr = this.charCodeAt(i);
+        hash = ((hash << 5) - hash) + chr;
+        hash |= 0; // Convert to 32bit integer
+    }
+    return hash.toString(32).replace("-", "");
+}
